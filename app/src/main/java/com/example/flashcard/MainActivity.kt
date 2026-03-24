ee3ab17fa4e63afb51c03b3c976fe55587858ebc
moroninavarro <moronialanis23@gmail.com>
2026-03-24 15:53:29 -0700

    Creating Buttons to navigate

--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.flashcard
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.flashcard.ui.theme.FlashCardTheme
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +35,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             FlashCardTheme {
 
+                val flashcards = remember { mutableStateListOf<Flashcard>()}
+
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
@@ -37,19 +44,28 @@ class MainActivity : ComponentActivity() {
                 )
                 {
                     composable("home") {
-                        MyHomeScreen(onNavigate = {
+                        MyHomeScreen(
+                            onCreateClick = {
                             navController.navigate("create")
-                        }
+                        },
+                            onStudyClick = {
+                                navController.navigate("study")
+                            }
                         )
+ //Route for my createScreen
                     }
                     composable("create") {
-                        MyCreateScreen(onNavigate = {
-                            navController.popBackStack()
-                        })
+                        MyCreateScreen(
+                            onNavigate = { navController.popBackStack() },
+                        flashcards = flashcards
+                        )
                     }
-
+//Route for my StudyScreen
                     composable ("study"){
-                        MyStudyScreen (){  }
+                        MyStudyScreen (
+                            flashcards = flashcards,
+                            onNavigate = { navController.popBackStack()
+                        })
                     }
                 }
             }
@@ -57,76 +73,131 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize() .padding(top=80.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "¡Welcome to FlashCards App! ",
-            fontSize = 24.sp,
-            modifier = modifier)
-    }
 
+// A Reusable Button to be able to call it in different
+// screens in my code
+@Composable
+fun ReusableButton(
+    text: String,
+    onClick: () -> Unit,
+    color: Color
+){
+ Button(
+     onClick = onClick,
+     colors = ButtonDefaults.buttonColors(
+         containerColor = Color.Black
+     )
+ ) {
+     Text(text = text, color = Color.White)
+ }
 }
+
+
 
 //This is my composable function to be able to
 //generate a button to create a Flashcard and
 //to display a message and the button with a
 //good layout.
 @Composable
-fun MyHomeScreen(onNavigate:() -> Unit){
+fun MyHomeScreen(onCreateClick:() -> Unit,
+                 onStudyClick: () -> Unit){
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Select an option to continue")
-        Button(onClick = onNavigate) {
-            Text("Create Flashcard", fontSize = 16.sp)
-        }
+        ReusableButton("CREATE", onCreateClick, Color.Cyan)
+        ReusableButton("STUDY", onStudyClick, Color.Red)
+
     }
 }
 
 @Composable
-fun MyStudyScreen(onNavigate:() -> Unit){
+fun MyStudyScreen(onNavigate: () -> Unit, flashcards: List<Flashcard>){
+    var currentIndex by remember { mutableStateOf(0) }
+    var showAnswer by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    )
+    {
         Text("Study My FlashCards")
-        Button(onClick = onNavigate) {
-            Text("Back",
-                fontSize = 16.sp)
+        if (flashcards.isNotEmpty()){
+            if (showAnswer){
+                Text(flashcards[currentIndex].answer)
+            }else{
+                Text(flashcards[currentIndex].question)
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally)
+            ){
+                ReusableButton("ANSWER", {showAnswer = !showAnswer},Color.Red)
+                ReusableButton("NEXT",onNavigate,Color.Red)
+            }
+
+
+        } else{
+            Text("First create your own FlashCards to start study :)")
         }
+
+        ReusableButton("BACK", onNavigate, Color.Green)
     }
 }
 
+data class Flashcard(
+    val question: String,
+    val answer: String
+)
 
+// My Create Screen
 @Composable
-fun MyCreateScreen(onNavigate:() -> Unit){
+fun MyCreateScreen(onNavigate: () -> Unit, flashcards: MutableList<Flashcard>){
+    var question by remember { mutableStateOf("") }
+    var answer by remember { mutableStateOf("") }
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Create a Flashcard to study")
-        Button(onClick = onNavigate) {
-            Text("Back",
-                fontSize = 16.sp)
-        }
+
+        TextField(
+            value = question,
+            onValueChange = { question = it },
+            label = { Text("Question") }
+        )
+        TextField(
+            value = answer,
+            onValueChange = { answer = it },
+            label = { Text("Answer") }
+        )
+        ReusableButton("SAVE", {
+            val newCard = Flashcard(question,answer)
+            flashcards.add(newCard)
+            question = ""
+            answer = ""
+        }, Color.Black)
+        ReusableButton("BACK", onNavigate, Color.Red)
+
     }
 }
 
 
 
-@Preview(
-    showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlashCardTheme {
-        Greeting("Moroni")
-    }
-}
+//@Preview(
+//    showBackground = true)
+//@Composable
+//fun MyCreateScreen(){
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.SpaceAround,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Text("Create a Flashcard to study")
+//
+//        ReusableButton("BACK") { }
+//    }
+//}
